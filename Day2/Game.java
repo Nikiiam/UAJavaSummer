67b79b2ee41a33ce109c542e8c695e365141dee8
Elena Kovalenko <fotoko22@gmail.com>
2021-06-27 13:25:14 +0300

    changes
    changes

---
+++ Day2/Game.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Game {
+
+
+        public static void main(String[] args){
+            // Генерируем случайное число
+            int number = (int)(Math.random()*100);
+
+            while(true){
+                // Вводим число с клавиатуры
+                Scanner sc = new Scanner(System.in);
+
+                System.out.print("Введите число, которое вы угадали (от 0 до 100):");
+
+                int gessnumber = sc.nextInt();
+                // Сравниваем с правильным числом
+                if(gessnumber > number){
+
+                    System.out.println("Cлишком большое");
+
+                }else if(gessnumber < number){
+
+                    System.out.println("Слишком маленькое");
+
+                }else {
+
+                    System.out.println("Ура, ты угадал!");
+
+                    break;
+                }
+            }
+        }
+    }
+
