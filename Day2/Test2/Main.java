d985a0a6aa7fe2639a4f81ff9def2a75f043dd16
Elena Kovalenko <fotoko22@gmail.com>
2021-07-02 14:29:38 +0300

    changes

---
+++ Day2/Test2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Initialization
+        Controller controller = new Controller(new Model(), new View());
+        // Run
+        controller.processUser();
+
+    }
+}
---
+++ Day2/Test2/Model.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Model {
+    private int value;
+    private int minValueCurrent;
+    private int maxValueCurrent;
+    private ArrayList<Integer> tries = new ArrayList<>();
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getMinValueCurrent() {
+        return minValueCurrent;
+    }
+
+    public int getMaxValueCurrent() {
+        return maxValueCurrent;
+    }
+
+    public void setMinValueCurrent(int minValueCurrent) {
+        this.minValueCurrent = minValueCurrent;
+    }
+
+    public void setMaxValueCurrent(int maxValueCurrent) {
+        this.maxValueCurrent = maxValueCurrent;
+    }
+
+    public ArrayList<Integer> getTries() {
+        return tries;
+    }
+
+    public ArrayList<Integer> setTries(int comparableValue) {
+        tries.add(comparableValue);
+        return tries;
+    }
+
+    // The Utility methods
+
+    public int generateNumber(int min, int max) {
+        //add 1 to max to make it inclusive
+        //add 1 to min to make it exclusive
+        value = ThreadLocalRandom.current().nextInt(min + 1 , max);
+        return value;
+    }
+
+    public boolean compareValue(int comparableValue) {
+        if(comparableValue > value && comparableValue < maxValueCurrent) {
+            maxValueCurrent = comparableValue;
+        } else if (comparableValue < value && comparableValue > minValueCurrent) {
+            minValueCurrent = comparableValue;
+        }
+        return comparableValue == value;
+    }
+
+
+
+}
